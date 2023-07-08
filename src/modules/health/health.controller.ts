56bd669cafb8eeec81097d653017a1c0f9dba8fd
nguyendang2909 <nguyendang2909@gmail.com>
2023-07-08 14:44:52 +0700

    feat: deploy

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Res } from '@nestjs/common';
 import { execSync } from 'child_process';
 import { Response } from 'express';
 
@@ -6,16 +6,21 @@ import { IsPublicEndpoint } from '../../commons/decorators/is-public.endpoint';
 
 @Controller('health')
 export class HealthController {
+  private logger = new Logger(HealthController.name);
   @Post('/deploy')
   @IsPublicEndpoint()
   create(@Res() res: Response, @Body() body: unknown) {
     console.log(body);
     res.status(200).json({ success: true });
 
-    execSync('git pull');
-    execSync('yarn');
-    execSync('yarn build');
-    execSync('pm2 restart server');
+    try {
+      execSync('git pull');
+      execSync('yarn');
+      execSync('yarn build');
+      execSync('pm2 restart server');
+    } catch (err) {
+      this.logger.error(`Failed to deploy`, err);
+    }
   }
 
   @IsPublicEndpoint()
