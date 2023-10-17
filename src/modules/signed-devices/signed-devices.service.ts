998c8307aa02de6c427f4e156705f4098bbbb428
nguyendang2909 <nguyendang2909@gmail.com>
2023-10-18 00:22:48 +0700

    feat: update device token

--- src/modules/signed-devices/signed-devices.service.ts
+++ src/modules/signed-devices/signed-devices.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
+import { HttpErrorMessages } from '../../commons/erros/http-error-messages.constant';
 import { ApiService } from '../../commons/services/api.service';
 import { ClientData } from '../auth/auth.type';
 import { SignedDeviceModel } from '../models/signed-device.model';
@@ -13,15 +14,32 @@ export class SignedDevicesService extends ApiService {
 
   async update(payload: UpdateSignedDeviceDto, client: ClientData) {
     const { _currentUserId } = this.getClient(client);
-    const signedDevice = await this.signedDeviceModel.findOneOrFail({
-      _userId: _currentUserId,
-      refreshToken: payload.refreshToken,
-    });
-    await signedDevice.updateOne({
-      $set: {
-        token: payload.deviceToken,
-        platform: payload.devicePlatform,
+    const signedDevice = await this.signedDeviceModel.findOneOrFail(
+      {
+        _userId: _currentUserId,
+        refreshToken: payload.refreshToken,
       },
-    });
+      {
+        _id: true,
+      },
+    );
+    const updateResult = await this.signedDeviceModel.model
+      .updateOne(
+        {
+          _id: signedDevice._id,
+        },
+        {
+          $set: {
+            token: payload.deviceToken,
+            platform: payload.devicePlatform,
+          },
+        },
+      )
+      .exec();
+    if (!updateResult.modifiedCount) {
+      throw new BadRequestException(
+        HttpErrorMessages['Update failed. Please try again.'],
+      );
+    }
   }
 }
