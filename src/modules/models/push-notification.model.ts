147440d93467592439a546a36ffaf42c2b17caea
Quynh Nguyen <quynh.nguyen01@your.rentals>
2023-10-18 13:30:44 +0700

    feat(YR-12508): webhook listing

---
+++ src/modules/models/push-notification.model.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { MongoDocument } from '../../commons/types';
+import { CommonModel } from './common-model';
+import {
+  PushNotification,
+  PushNotificationDocument,
+} from './schemas/push-notification.schema';
+import { UserModel } from './user.model';
+
+@Injectable()
+export class MessageModel extends CommonModel {
+  constructor(
+    @InjectModel(PushNotification.name)
+    public readonly pushNotificationModel: Model<
+      MongoDocument<PushNotificationDocument>
+    >,
+    private readonly userModel: UserModel,
+  ) {
+    super();
+  }
+
+  async createOne(
+    payload: Partial<PushNotification>,
+  ): Promise<PushNotificationDocument> {
+    const result = await this.pushNotificationModel.create(payload);
+    return result.toJSON();
+  }
+}
