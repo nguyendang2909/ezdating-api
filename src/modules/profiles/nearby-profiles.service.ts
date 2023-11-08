0172892bd2392ce0e2643f44f633968b6423c926
nguyendang2909 <nguyendang2909@gmail.com>
2023-11-08 21:13:44 +0700

    feat: file structure by roles

--- src/modules/profiles/nearby-profiles.service.ts
+++ src/modules/profiles/nearby-profiles.service.ts
@@ -4,7 +4,7 @@ import moment from 'moment';
 import { APP_CONFIG } from '../../app.config';
 import { ERROR_MESSAGES } from '../../commons/messages';
 import { ApiService } from '../../commons/services/api.service';
-import { NearbyUserCursor, PaginatedResponse, Pagination } from '../../types';
+import { PaginatedResponse, Pagination } from '../../types';
 import { ClientData } from '../auth/auth.type';
 import { Profile, ProfileModel } from '../models';
 import { FindManyNearbyProfilesQuery } from './dto';
@@ -20,14 +20,9 @@ export class NearbyProfilesService extends ApiService {
     queryParams: FindManyNearbyProfilesQuery,
     client: ClientData,
   ): Promise<PaginatedResponse<Profile>> {
-    const { _next } = queryParams;
-
-    const cursor = _next ? this.getCursor(_next) : undefined;
-    const minDistance = cursor ? cursor.minDistance : undefined;
-    const excludedUserIds = cursor
-      ? cursor.excludedUserIds?.map((e) => this.getObjectId(e))
-      : undefined;
     const { _currentUserId } = this.getClient(client);
+    const { _next } = queryParams;
+    const cursor = _next ? this.getCursor(_next) : undefined;
     const {
       geolocation,
       filterMaxAge,
@@ -65,23 +60,14 @@ export class NearbyProfilesService extends ApiService {
           },
           distanceField: 'distance',
           // distanceMultiplier: 0.001,
-          ...(minDistance
+          ...(cursor
             ? {
-                minDistance: minDistance,
+                minDistance: cursor,
               }
-            : {}),
+            : { minDistance: 0.00000000001 }),
           // TODO: uncoment this line (important for launch)
           // maxDistance: filterMaxDistance,
           query: {
-            _id: {
-              ...(excludedUserIds?.length
-                ? {
-                    $nin: [_currentUserId, ...excludedUserIds],
-                  }
-                : {
-                    $ne: _currentUserId,
-                  }),
-            },
             mediaFileCount: { $gt: 0 },
             // lastActivatedAt: {
             //   $gt: moment().subtract(7, 'd').toDate(),
@@ -92,12 +78,6 @@ export class NearbyProfilesService extends ApiService {
               $lte: moment().subtract(filterMinAge, 'years').toDate(),
             },
           },
-        },
-      },
-      {
-        $sort: {
-          distance: 1,
-          _id: 1,
         },
       },
       { $limit: this.limitRecordsPerQuery },
@@ -121,22 +101,40 @@ export class NearbyProfilesService extends ApiService {
       return { _next: null };
     }
     const lastData = data[dataLength - 1];
-    const minDistance = data[data.length - 1].distance;
-    const excludedUserIds = data
-      .filter((e) => e.distance === minDistance)
-      .map((e) => e._id.toString());
-
     return {
-      _next: this.encodeFromObj({
-        minDistance: lastData.distance,
-        excludedUserIds,
-      }),
+      _next: this.encodeFromString(`${lastData.distance}`),
     };
   }
 
-  protected getCursor(_cursor: string): NearbyUserCursor {
-    const cursor = this.decodeToObj<NearbyUserCursor>(_cursor);
-
-    return cursor;
+  protected getCursor(_cursor: string): number {
+    const cursor = this.decodeToString(_cursor);
+    return +cursor;
   }
+
+  // public getPagination(
+  //   data: (Profile & { excludedUserIds?: string[] })[],
+  // ): Pagination {
+  //   const dataLength = data.length;
+  //   if (!dataLength || dataLength < this.limitRecordsPerQuery) {
+  //     return { _next: null };
+  //   }
+  //   const lastData = data[dataLength - 1];
+  //   const minDistance = data[data.length - 1].distance;
+  //   const excludedUserIds = data
+  //     .filter((e) => e.distance === minDistance)
+  //     .map((e) => e._id.toString());
+
+  //   return {
+  //     _next: this.encodeFromObj({
+  //       minDistance: lastData.distance,
+  //       excludedUserIds,
+  //     }),
+  //   };
+  // }
+
+  // protected getCursor(_cursor: string): NearbyUserCursor {
+  //   const cursor = this.decodeToObj<NearbyUserCursor>(_cursor);
+
+  //   return cursor;
+  // }
 }
